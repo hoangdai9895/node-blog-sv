a954f2d809024adadc77f91f5474dcd8e9a36dcc
unknown <hoangdai9895@gmail.com>
2019-04-11 16:11:07 +0700

    cors auth

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ require('./config/passport')(passport);
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
